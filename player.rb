0355001322c27149560e3f6bf0d1524ad55d55d0
Sydney Brown <bsydney95@gmail.com>
2022-01-24 16:14:12 -0500

    Created a player class with 'name' and 'mark' instance variables. Only two instances of the player class can be created (only two players in tic-tac-toe) and each instance is automatically assigned a 'name' (Player 1 or Player 2) and a 'mark' (X and O respectively).

---
+++ player.rb
@@ -0,0 +1,20 @@
+class Player
+    attr_reader :name, :mark
+    @@instances = 0
+
+    def initialize
+        if @@instances >= 2
+            raise "Only two players per game."
+        elsif @@instances == 1
+            @name = "Player 2"
+            @mark = "O"
+            @@instances += 1
+        else
+            @name = "Player 1"
+            @mark = "X"
+            @@instances += 1
+        end
+    end
+    
+end
+
