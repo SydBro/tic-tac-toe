9d17e5660d8042be3768ff947a16d285de389904
Sydney Brown <bsydney95@gmail.com>
2022-01-25 16:38:12 -0500

    The game now checks for win states (so far row and column wins only) and the game will end if the board fills before a win condition is met.

--- game.rb
+++ game.rb
@@ -1,42 +1,49 @@
 class Game
     require_relative 'player.rb'
     require_relative 'board.rb'
-    attr_reader :player1, :player2, :board_state
+    attr_reader :player1, :player2, :board
 
     def initialize
         @player1 = Player.new
         @player2 = Player.new
-        @board_state = Board.new
+        @board = Board.new
         @end_game = false
     end
 
     def play
         players = [@player1, @player2]
-        until (@board_state.check_full == true)
+        until (@board.check_full == true || @end_game == true)
             players.each do |player|
-                p @board_state.board #displays the board state at the beginning of each player's turn
-                p "#{player.name}, your move. Which row do you want to put your #{player.mark}?"
-                player_row = gets.chomp.to_i
-                p "Which column do you want to put your #{player.mark}?"
-                player_column = gets.chomp.to_i
-                until (player_row.to_s.match(/[0-2]/) && player_column.to_s.match(/[0-2]/) && @board_state.board[player_row][player_column] == nil)
-                    p "Invalid placement. Enter a number 0 through 2 on an unoccupied space."
-                    p "#{player.name}, which row do you want to put your #{player.mark}?"
+                player_win = @board.win_conditions(player.name, player.mark)
+                if (player_win == true)
+                    @board.board_state
+                    @end_game = true
+                    break
+                elsif (@board.check_full == true && @end_game == false)
+                    @board.board_state
+                    puts "Game over; no more space on the board."
+                    break
+                else
+                    @board.board_state #displays the board state at the beginning of each player's turn
+                    p "#{player.name}, your move. Which row do you want to put your #{player.mark}?"
                     player_row = gets.chomp.to_i
                     p "Which column do you want to put your #{player.mark}?"
                     player_column = gets.chomp.to_i
+                    until (player_row.to_s.match(/[0-2]/) && player_column.to_s.match(/[0-2]/) && @board.board[player_row][player_column] == nil)
+                        p "Invalid placement. Enter a number 0 through 2 on an unoccupied space."
+                        p "#{player.name}, which row do you want to put your #{player.mark}?"
+                        player_row = gets.chomp.to_i
+                        p "Which column do you want to put your #{player.mark}?"
+                        player_column = gets.chomp.to_i
+                    end
+                    @board.add_mark(player.mark, player_row, player_column)
+                    @board.win_conditions(player.name, player.mark)
                 end
-                @board_state.add_mark(player.mark, player_row, player_column)
-                @board_state.check_full
             end
         end
     end
 
-=begin - methods to -be implemented
-    def win_conditions()
-        
-    end
-
+=begin - methods to be implemented
     def end_game
         
     end
@@ -46,5 +53,3 @@ end
 
 new_game = Game.new
 new_game.play
-
-
